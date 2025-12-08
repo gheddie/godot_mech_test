c9cffe166feb1cbd52b45b3d0746585ab36e4559
Stefan Schulz <stefan.schulz.1976@googlemail.com>
2025-12-08 10:45:37 +0100

    prepared and integrated walk animation

--- Robot.gd
+++ Robot.gd
@@ -10,21 +10,16 @@ const TURN_SPEED = 0.5
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	"""
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		"""
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("moveBackward", "moveForward", "null", "null")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		walk()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		idle()
 	handleTurn(delta)
 	move_and_slide()
 
@@ -35,13 +30,18 @@ func handleTurn(_delta: float) -> void:
 	elif  Input.is_action_pressed("turnRight"):
 		rotation.y -= TURN_SPEED * _delta
 		tipple()
-	else:
-		idle()
 
 func tipple() -> void:
-	animationTree["parameters/conditions/idle"] = false
-	animationTree["parameters/conditions/tipple"] = true	
-	
+	animationTree["parameters/conditions/tipple"] = true
+	animationTree["parameters/conditions/idle"] = false	
+	animationTree["parameters/conditions/walk"] = false	
+		
 func idle() -> void:
 	animationTree["parameters/conditions/idle"] = true
 	animationTree["parameters/conditions/tipple"] = false	
+	animationTree["parameters/conditions/walk"] = false	
+
+func walk() -> void:
+	animationTree["parameters/conditions/walk"] = true	
+	animationTree["parameters/conditions/idle"] = false
+	animationTree["parameters/conditions/tipple"] = false		
